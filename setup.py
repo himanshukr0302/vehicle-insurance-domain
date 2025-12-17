370151e1d0d0fbcd335ce754cfbe7d658fdfd7f8
himanshu@cachyos <student030205@gmail.com>
2025-12-17 16:01:44 +0530

    project structure ready

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Himanshu Kumar",
+    author_email="himanshukr0302@gmail.com",
+    packages=find_packages()
+)
